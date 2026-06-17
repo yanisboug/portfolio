f916b11b93d6255fc35c239cdab30be278dc5de1
yanisboug <yanis.bougchiche@polymtl.ca>
2026-06-16 23:32:11 -0400

    config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,14 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+
+import svelte from "@astrojs/svelte";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	vite: {
+		plugins: [tailwindcss()],
+	},
+
+	integrations: [svelte()],
+});
